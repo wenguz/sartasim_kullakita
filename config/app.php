eb35284c4c4eb2c470fa2fc76e98f024fabb9e12
wenguz <wenguz1518@gmail.com>
2019-03-13 23:47:02 -0500

    Se quito el provider y alias de shinobi

--- config/app.php
+++ config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Caffeinated\Shinobi\ShinobiServiceProvider::class,
 
 
     ],
@@ -227,7 +226,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Shinobi' => Caffeinated\Shinobi\Facades\Shinobi::class,
     ],
 
 ];
